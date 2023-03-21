125633915ce1db6ad6e15c1aa98102af8c796c47
dr_noob <dev.arun2302@gmail.com>
2023-03-21 17:59:58 +0530

    pod deletion solution

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -11,13 +10,19 @@ import (
 	"syscall"
 	"time"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+const TypeReplicaSet = "ReplicaSet"
+const TypeDeployment = "Deployment"
+
+var myPodName string
+var myPodNameSpace string
 
 func contains(In []string, find string) bool {
 	for _, v := range In {
@@ -28,12 +33,35 @@ func contains(In []string, find string) bool {
 	return false
 }
 
-func delDeployments(ctx context.Context, clientset *kubernetes.Clientset, namespace string) {
+func isPartOfDeployment(ctx context.Context, clientset *kubernetes.Clientset, pod *v1.Pod, ownerRefs []metav1.OwnerReference) bool {
+	if len(ownerRefs) == 0 {
+		return false
+	}
 
-	deployments, err := clientset.AppsV1().Deployments(namespace).List(ctx, metav1.ListOptions{})
+	ownerRef := ownerRefs[0]
+
+	switch ownerRef.Kind {
+	case TypeReplicaSet:
+
+		replica, repErr := clientset.AppsV1().ReplicaSets(pod.Namespace).Get(ctx, ownerRef.Name, metav1.GetOptions{})
+		if repErr != nil {
+			log.Printf("Error getting replica info : [%v]\n", repErr.Error())
+			return false
+		}
+
+		repOwnRef := replica.OwnerReferences
+
+		if len(repOwnRef) > 0 && repOwnRef[0].Kind == TypeDeployment {
+			return true
+		}
+	}
+	return false
+}
+
+func delPod(ctx context.Context, clientset *kubernetes.Clientset) {
+	pods, err := clientset.CoreV1().Pods("").List(ctx, metav1.ListOptions{})
 	if err != nil {
-		log.Printf("Error getting deployments : [%v]\n", err)
-		return
+		log.Printf("Error getting pod info. Err : [%v]\n", err.Error())
 	}
 
 	deps, ok := os.LookupEnv("SKIP_DEPLOYMENTS")
@@ -41,38 +69,41 @@ func delDeployments(ctx context.Context, clientset *kubernetes.Clientset, namesp
 	var depsArr []string
 	if ok {
 		depsArr = strings.Split(deps, ",")
+		log.Println("Deployments to skip are:", deps)
 	}
 
-	for _, d := range deployments.Items {
+	for _, pod := range pods.Items {
 
-		if contains(depsArr, d.Name) {
-			log.Printf("deployment [%v] set to skip", d.Name)
+		if contains(depsArr, pod.Namespace) {
+			log.Printf("Skipped: Pod [%v] belong to skipped deployment : [%v]\n", pod.Name, pod.Namespace)
 			continue
 		}
 
-		if d.Namespace == "kube-system" {
-			log.Printf("deployment [%v] is part of kube-system. Skipping", d.Name)
+		// do not delete the pods which belongs to k8 related deployments
+		if pod.Namespace == "kube-system" {
 			continue
 		}
 
-		deletePolicy := metav1.DeletePropagationForeground
-		err := clientset.AppsV1().Deployments(d.Namespace).Delete(ctx, d.Name, metav1.DeleteOptions{
-			PropagationPolicy: &deletePolicy,
-		})
+		if isPartOfDeployment(ctx, clientset, &pod, pod.OwnerReferences) {
 
-		if err != nil {
-			log.Printf("Error deleting deployment : [%v]. Err : [%v]\n", d.Name, err)
-			continue
+			if strings.EqualFold(myPodName, pod.Name) && strings.EqualFold(myPodNameSpace, pod.Namespace) {
+				log.Printf("Skipped: Pod [%v] cannot delete itself\n", pod.Name)
+				continue
+			}
+
+			// delete the pod
+			err := clientset.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, metav1.DeleteOptions{})
+			if err != nil {
+				log.Printf("Error deleting pod [%v]. Err: [%v]", pod.Name, err.Error())
+				continue
+			}
+			log.Printf("Deleted: pod name : [%v], namespace: [%v]\n", pod.Name, pod.Namespace)
 		}
-
-		fmt.Printf("Deleted deployment : [%v].\n", d.Name)
 	}
-
 }
 
 func main() {
-	ctx := context.Background()
-
+	ctx, stopF := context.WithCancel(context.Background())
 	var config *rest.Config
 	var err error
 	kubeconfig := flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
@@ -92,6 +123,17 @@ func main() {
 		log.Fatalf("Error gettting configs : Err : [%v]\n", err.Error())
 	}
 
+	var ok bool
+	myPodName, ok = os.LookupEnv("POD_NAME")
+	if !ok {
+		log.Fatalf("POD_NAME env not set")
+	}
+
+	myPodNameSpace, ok = os.LookupEnv("POD_NAMESPACE")
+	if !ok {
+		log.Fatalf("POD_NAMESPACE env not set")
+	}
+
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
@@ -101,15 +143,14 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	delDeployments(ctx, clientset, "")
-
+	delPod(ctx, clientset)
 	for {
 		select {
 		case <-c:
+			stopF()
 			return
 		case <-time.After(time.Duration(*pollDuration) * time.Second):
-			delDeployments(ctx, clientset, "")
+			delPod(ctx, clientset)
 		}
 	}
-
 }
